ae73306ce82d6e626465fd867c243420bcb0ffe7
TinCode1998 <nelsonmouatvergara@gmail.com>
2024-10-14 17:10:49 -0300

    se cambio nav

--- pagina/menusAdm/navMenus/sideNav.php
+++ pagina/menusAdm/navMenus/sideNav.php
@@ -225,16 +225,48 @@
 
         <?php
         if ($_SESSION["usuario"] == "tinBarlovento") {
-            echo include_once "vermemoria.php";
+            $mbytes = number_format($tamano / (1024 * 1024), 2);
+            $totalPorct = 0;
+            $totalPorct = (100 * $mbytes) / $tamanoMax;
+
+        ?>
+            <div class="container-fluid">
+                Espacio de la base de datos
+                <br>
+                <div class="progress" style="height: 15px; border-radius: 10px;">
+                    <?php
+                    if ($totalPorct < 50) {
+                        $colorLetra = '';
+                        $palabra = '';
+                    }
+                    if ($totalPorct >= 50 && $totalPorct < 90) {
+                        $colorLetra = 'bg-warning';
+                        $palabra = 'Queda poca espacio';
+                    }
+                    if ($totalPorct >= 90) {
+                        $colorLetra = 'bg-danger';
+                        $palabra = 'Esta lleno espacio';
+                    }
+                    echo "<div class='progress-bar " . $colorLetra . "' role='progressbar' style='width: " . $totalPorct . "%; border-radius: 10px;' aria-valuenow='25' aria-valuemin='0' aria-valuemax='100'>
+                " . $palabra . "
+                </div>";
+                    ?>
+                </div>
+                <?php
+                echo $mbytes . " MB / " . $tamanoMax . " MB";
+                ?>
+            </div>
+        <?php
         }
+
         ?>
 
         <?php
-        if ($_SERVER["REQUEST_URI"] == $sacar . "pagina/menusAdm/menu.php") {
-            echo '<img src="../../image/logo_barlovento.png" class="img-fluid" alt="logo_CentroBarlovento">';
-        } else if ($_SERVER["REQUEST_URI"] !== $sacar . "pagina/menusAdm/menu.php") {
-            echo '<img src="../../../image/logo_barlovento.png" class="img-fluid" alt="logo_CentroBarlovento">';
-        }
+        // if ($_SERVER["REQUEST_URI"] == $sacar . "pagina/menusAdm/menu.php") {
+        //     echo '<img src="../../image/logo_barlovento.png" class="img-fluid" alt="logo_CentroBarlovento">';
+        // } else if ($_SERVER["REQUEST_URI"] !== $sacar . "pagina/menusAdm/menu.php") {
+        //     echo '<img src="../../../image/logo_barlovento.png" class="img-fluid" alt="logo_CentroBarlovento">';
+        // }
         ?>
         <span>© 2018 - <?php echo date("Y"); ?></span>
         <span><?php echo $verServer; ?></span>
